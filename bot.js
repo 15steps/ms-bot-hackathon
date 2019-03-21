1319347977043eec0ca5ae19130e52ed9fd92e29
Wellington Felix <wfmf@cin.ufpe.br>
2019-03-21 11:22:10 -0300

    add spotify data

--- bot.js
+++ bot.js
@@ -2,6 +2,16 @@
 // Licensed under the MIT License.
 
 const { ActivityTypes, CardFactory } = require('botbuilder');
+const SpotifyWebApi = require('spotify-web-api-node');
+
+// credentials are optional
+const spotifyApi = new SpotifyWebApi({
+  clientId: '3f2026a5f25148d2a24993bedb4e7186',
+  clientSecret: 'e23caf35d45c4778a2a2087e29bebd12',
+//   redirectUri: 'http://www.example.com/callback'
+});
+
+spotifyApi.setAccessToken('BQDax6-BmqMg6nkFGbc254p8fdo3KsPZlP9jVuwJl2UV5I8ng9StBvSQFQib3lrOJ0rweAEGcyl3l-vUkx5TucSIv65ynLbP_sBKBFooG-R5v2ItZrMoD83p7HJ6WuEyAwHf6ja5cUQgB7c75oNk_PVxhpBE3Lt6');
 
 class MyBot {
     /**
@@ -11,10 +21,17 @@ class MyBot {
     async onTurn(turnContext) {
         // See https://aka.ms/about-bot-activity-message to learn more about the message and other activity types.
         if (turnContext.activity.type === ActivityTypes.Message) {
+            try {
+                const description = turnContext.activity.text;
+                const { body } = await spotifyApi.searchPlaylists(description);
+                const playlist = body.playlists.items[0];
+                const { name, images, tracks: { total }, external_urls: { spotify } } = playlist;
+                const card = this.generateCard(name, `${total} tracks` , "Tracks: ", images[0].url, "Play on Spotify", spotify || 'https://google.com')
+                await turnContext.sendActivity({ attachments: [card] });
+            } catch (e) {
+                console.error(e);
+            }
             // await turnContext.sendActivity(`You said '${ turnContext.activity.text }'`);
-            const link = 'https://sec.ch9.ms/ch9/7ff5/e07cfef0-aa3b-40bb-9baa-7c9ef8ff7ff5/buildreactionbotframework_960.jpg'
-            const card = this.generateCard("Title", "Subtitle" , "Text here", link, "Play on Spotify", link)
-            await turnContext.sendActivity({ attachments: [card] });
         } else {
             await turnContext.sendActivity(`[${ turnContext.activity.type } event detected]`);
         }
